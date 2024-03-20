7d1bca73129f086c4fd6111a5f9a592297f40fc5
Ishita Chauhan <96109876+Ishita-Chauhan@users.noreply.github.com>
2024-03-20 15:40:47 +0530

    templates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import bcrypt
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.secret_key = '123456'
 
 users = {}
